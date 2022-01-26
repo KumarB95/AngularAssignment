5bfde020476ea4908e206df4f4872713bac1e84e
KumarB95 <kumarbalingal@gmail.com>
2022-01-26 21:12:17 +0530

    Added : regex validations for entered name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent {
   url="https://www.bridgelabz.com/"
 
   userName:string="";
+  nameError:string="";
 
 
   ngOnInit(){
@@ -25,5 +26,15 @@ export class AppComponent {
   onClick($event:any){
     console.log("Save button is clicked!" ,$event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event:any){
+    console.log("Change event occured !", $event.data);
+    const nameRegex= RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is incorrect!"
   }
 }
